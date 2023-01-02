199568e40c58ad15ba760fda9615cd9e246aa7c4
StepanPyJunior <stepan.musil1@seznam.cz>
2023-01-02 13:30:16 +0100

    prvy commit, pridal jsem kviz.py

---
+++ Kviz.py
@@ -0,0 +1,18 @@
+print("Vítej v programu Kvíz")
+
+c = 0
+a = input("Kolik je 2 + 2? ")
+if a == "4":
+    print("Správná odpověď")
+    c += 1
+else:
+    print("Špatná odpověď")
+
+b = input ("Kolik je 2 x 3? ")
+if b == str(2 * 3):
+    print("Správná odpověď")
+    c += 1
+else:
+    print("Špatná odpověď")
+
+print("Odpověděl jsi správně na " + str(c) + " otázky a tvá úspěšnost je " + str(round(float(c)/2*100, 1)) + "%")
