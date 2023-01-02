f65e1870c61884246e719955b75136c7d0b74ffb
StepanPyJunior <stepan.musil1@seznam.cz>
2023-01-02 13:36:08 +0100

    Přidal jsem třetí otázku

--- Kviz.py
+++ Kviz.py
@@ -15,4 +15,11 @@ if b == str(2 * 3):
 else:
     print("Špatná odpověď")
 
-print("Odpověděl jsi správně na " + str(c) + " otázky a tvá úspěšnost je " + str(round(float(c)/2*100, 1)) + "%")
+d = input ("Kolik je 3 x 3? ")
+if d == str(3 * 3):
+    print("Správná odpověď")
+    c += 1
+else:
+    print("Špatná odpověď")
+
+print("Odpověděl jsi správně na " + str(c) + " otázky a tvá úspěšnost je " + str(round(float(c)/3*100, 1)) + "%")
