1738558edea7a2911b41909241480347921e4f72
StepanPyJunior <stepan.musil1@seznam.cz>
2023-01-03 17:53:17 +0100

    Definoval jsem třídu Postavy a přiřadil jim hodnoty jméno, cech.

--- Hra.py
+++ Hra.py
@@ -1,5 +1,13 @@
 import random
 
+class Postavy:
+    def __init__(self, jmeno, cech):
+        self.jmeno = jmeno
+        self.cech = cech
+
+    def informace(self):
+        print("\nJméno postavy:", self.jmeno, "\nČlenem cechu:", self.cech + "ů")
+
 seznam_cechu = {1: "Válečník", 2: "Kouzelník", 3: "Zloděj"}
 
 jmeno_hrace = input("Vítej ve hře na hrdiny. Jak se jmenuješ?\n")
@@ -19,5 +27,9 @@ while True:
     else:
         print("Zadej číselnou hodnotu!")
         continue
+
+hlavni_hrdina = Postavy(jmeno_hrace, cech_hrace)
+
+
 
 
