55afb9904abe23278b955533c1a53e68f8e9c3ec
StepanPyJunior <stepan.musil1@seznam.cz>
2023-01-03 16:32:49 +0100

    Vytvořil jsem projekt Hra

---
+++ Hra.py
@@ -0,0 +1,23 @@
+import random
+
+seznam_cechu = {1: "Válečník", 2: "Kouzelník", 3: "Zloděj"}
+
+jmeno_hrace = input("Vítej ve hře na hrdiny. Jak se jmenuješ?\n")
+print("\nVýborně, od teď budeš znám jako", jmeno_hrace)
+
+while True:
+    cech_hrace = input("\nNyní si vyber cech\n1. Cech válečníků\n2. Cech kouzelníků\n3. Cech zlodějů\n")
+    if cech_hrace.isdigit():
+        cech_hrace = int(cech_hrace)
+        if cech_hrace not in seznam_cechu:
+            print("Zadej číslo od 1 do 3.")
+            continue
+        else:
+            cech_hrace = seznam_cechu[cech_hrace]
+            print("\nPřidal jsi se k cechu", cech_hrace + "ů.")
+            break
+    else:
+        print("Zadej číselnou hodnotu!")
+        continue
+
+
